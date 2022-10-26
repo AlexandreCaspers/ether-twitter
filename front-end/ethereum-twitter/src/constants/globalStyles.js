d93da6d46e00de817e0733d81076fe6a14f45f0b
Martin Sejas <mhsejas@gmail.com>
2022-10-26 22:28:24 +0200

    Installation of styling library, made global style, applied twitter color and logo, created readme

--- front-end/ethereum-twitter/src/constants/globalStyles.js
+++ front-end/ethereum-twitter/src/constants/globalStyles.js
@@ -1,3 +1,6 @@
+import {createGlobalStyle} from 'styled-components'; 
+
+const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -5,9 +8,16 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  padding: 0; 
+box-sizing: border-box; 
+background-color: #1DA1F2; //copying twitter's original color
 }
 
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+`
+
+export default GlobalStyle; 
